3e4cc88357442df5f435b9b215b095b9f81ef79e
orange95 <b02611006@ntu.edu.tw>
2015-08-12 10:43:51 +0800

    213

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QString"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString str = "hello";
+    qDebug() << str;
+    str.prepend("123");
+   qDebug() << str;
+   str.append("456");
+    qDebug() << str;
+
+    QString str2 = "yes ";
+    QString str3 = str+str2;
+    qDebug() << str3;
+
 }
 
 MainWindow::~MainWindow()
